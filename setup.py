3467173f6c95b924b959c84fba316b51abe4f3c5
John Kitonyo <johnkilonzi@outlook.com>
2025-06-12 20:36:59 -0400

    increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="ksubmit",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
